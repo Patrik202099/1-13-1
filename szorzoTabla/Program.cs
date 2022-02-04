98b0e4e0be1bb5f234aa414098fd68930f290f81
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-04 09:31:05 +0100

    Szorzótábla

---
+++ szorzoTabla/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szorzoTabla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int elsoSzam, masodikSzam;
+
+            Console.WriteLine("Helló!");
+
+            Console.WriteLine("Add meg az első számot:");
+            elsoSzam = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Add meg a második számot:");
+            masodikSzam = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(elsoSzam*masodikSzam);
+            Console.ReadKey();
+        }
+    }
+}
