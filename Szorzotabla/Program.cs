c10486d6891dd98b0d2e172a48451f34b0485e07
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-07 13:11:19 +0100

    jó

---
+++ Szorzotabla/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szorzotabla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum, secondNum;
+            Random rnd = new Random();
+
+            Console.WriteLine("Üdvözöllek!");
+            Console.WriteLine("Add meg az első számot!: ");
+            
+            //if (args.Length == 0)
+            //{
+            //    firstNum = rnd.Next(0, 101);
+            //    secondNum = rnd.Next(0, 101);
+            //}
+
+            firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Add meg a második számot!: ");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("A két szám szorzata: {0}", firstNum * secondNum);
+
+            Console.ReadKey();
+        }
+    }
+}
