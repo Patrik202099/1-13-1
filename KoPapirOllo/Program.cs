20a5a0e174e9bd07b829e6853f14bf9ba3483860
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-07 14:49:06 +0100

    jó

---
+++ KoPapirOllo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoPapirOllo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            string playerChoice = "";
+            string compChoice = "";
+
+            int playerPoint = 0;
+            int compPoint = 0;
+
+            Console.WriteLine("Üdvözöllek!");
+
+            bool g = true;
+            do
+            {
+                Console.WriteLine("Szeretnél még játszani? (i / n)");
+
+                if (Console.ReadKey(true).KeyChar == 'n')
+                {
+                    g = false;
+                    Console.WriteLine("Viszlát!");
+                    break; 
+                }
+            } while (g == true);
+
+            Console.WriteLine("Mit választasz?: ");
+            switch (Console.ReadKey(true).KeyChar)
+
+            {
+                case 'k':
+                    playerChoice = "kő";
+                    break;
+
+                case 'p':
+                    playerChoice = "papír";
+                    break;
+
+                case 'o':
+                    playerChoice = "olló";
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
