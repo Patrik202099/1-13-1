b6e1be7156063977ebf1657b42a4fa772c663b56
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-09 11:40:34 +0100

    jó

---
+++ Szamologep/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szamologep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum, secondNum;
+            char op;
+
+            Console.WriteLine("Üdvözöllek!");
+
+            Console.WriteLine("Add meg az első számot!: ");
+            firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Add meg a műveleti jelet: ");
+            op = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Add meg a második számot!: ");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
