37bfb0339c91509275917c5041bc3edadb9ac06c
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-09 13:25:59 +0100

    jó

---
+++ gyakorlas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyakorlas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string beker;
+            int homerseklet;
+
+            Console.WriteLine("Adj meg egy hőmérsékletet.");
+            homerseklet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Celsiusból - Fahrenheitbe, vagy Fahrenheitből - Celsiusba váltanál? (e / m) ");
+            
+            switch (Console.ReadKey(true).KeyChar)
+            {
+                case 'e':
+                    Console.WriteLine(homerseklet + " celsius pontosan");
+                    break;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
