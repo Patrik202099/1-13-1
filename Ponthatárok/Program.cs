4f826545125e1d1a7000fbcbdd379d64e271628a
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-15 19:33:03 +0100

    Pontkalkulátor

---
+++ Ponthatárok/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponthatárok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ponthatar;
+
+            Console.WriteLine("Ponthatár kalkulátor");
+
+            Console.WriteLine("Kérlek add meg az elért pontszámot: ");
+            ponthatar = Convert.ToInt32(Console.ReadLine());
+
+            switch (ponthatar)
+            {
+                case  < 50:
+                    Console.WriteLine("1-es");
+                    break;
+
+                case ponthatar 
+            }
+        }
+    }
+}
