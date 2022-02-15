7bee229c984e01dcdd255367df31215edde6a557
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-15 19:23:04 +0100

    BMI Kalkulátor, szöveges indexekkel.

--- TestTomegIndex/Program.cs
+++ TestTomegIndex/Program.cs
@@ -23,10 +23,29 @@ namespace TestTomegIndex
             Console.WriteLine("A BMI értéked: " + Math.Round(testsuly / (magassag * magassag) * 10000,2));
             bmi = Convert.ToDouble(Console.ReadLine());
             
-            if (bmi > 16)
+            if (bmi < 16)
             {
-                Console.WriteLine("Súlyosan sovány vagy!");
+                Console.WriteLine("Súlyosan sovány vagy.");
             }
+
+            else if (bmi > 16 && bmi < 17)
+            {
+                Console.WriteLine("Mérsékelten sovány vagy.");
+            }
+
+            else if (bmi > 17 && bmi < 19)
+                Console.WriteLine("Enyhén sovány vagy.");
+
+            else if (bmi > 19 && bmi < 25)
+            {
+                Console.WriteLine("Normális testsúllyal rendelkezel.");
+            }
+
+            else if (bmi > 25)
+            {
+                Console.WriteLine("Elhízott vagy.");
+            }
+
             Console.ReadKey();
         }
     }
