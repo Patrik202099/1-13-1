491662a4e0e696ac2b8ffac99fb5139e0fe5a91e
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-15 11:40:04 +0100

    jó

---
+++ TestTomegIndex/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTomegIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double testsuly, bmi;
+            int magassag;
+            Console.WriteLine("Üdvözöllek!");
+
+            Console.WriteLine("Add meg a testsúlyod: ");
+            testsuly = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Add meg a magasságod: ");
+            magassag = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A BMI értéked: " + Math.Round(testsuly / (magassag * magassag) * 10000,2));
+            bmi = Convert.ToDouble(Console.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
