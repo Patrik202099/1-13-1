ad88b5c6da8700138eaef226f6d6c1237c6f2e39
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-16 12:13:50 +0100

    Ponttávolság kalkulátor

---
+++ PontokTavolsaga/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontokTavolsaga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float x1, x2, y1, y2, eredmeny;
+
+            Console.WriteLine("Helló!");
+
+            Console.WriteLine("Add meg az X1-et.");
+            x1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Add meg az X2-et.");
+            x2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Add meg az Y1-et.");
+            y1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Add meg az Y2-et.");
+            y2 = float.Parse(Console.ReadLine());
+
+           
+            Console.WriteLine("A pontok távolsága: " + Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
+            Console.ReadKey();
+        }
+    }
+}
