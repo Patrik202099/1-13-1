58a5a323288ae2f017e9b26106fee83b007c406e
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-17 19:51:42 +0100

    Random Number Generator between 2 user inputs.

---
+++ VeletlenSzamokListaja/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeletlenSzamokListaja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int min,
+                max,
+                menny = 0,
+                veletlen;
+
+            Console.WriteLine("Első érték: ");
+            min = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Második érték: ");
+            max = Convert.ToInt32(Console.ReadLine());
+
+            
+            
+            while (menny < 3)
+            {
+                veletlen = random.Next(min, max);
+                menny++;
+                Console.WriteLine("\n" + veletlen);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
