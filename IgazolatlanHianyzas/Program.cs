d038c10934c35a63378cdb2756661543af8dce5c
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-17 13:59:18 +0100

    half

---
+++ IgazolatlanHianyzas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgazolatlanHianyzas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hianyzasok,
+                szuletes;
+
+            Console.WriteLine("Add meg az igazolatlan hiányzásaidat: ");
+            hianyzasok = Convert.ToInt32(Console.ReadLine());
+
+            if (hianyzasok >= 10)
+            {
+                Console.WriteLine("Add meg a születési dátumod: (EV/HONAP/NAP)");
+                szuletes = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Igazolatlan hiányzásaid száma: " + szuletes);
+            }
+
+            if (hiany)
+
+            Console.ReadKey();
+        }
+    }
+}
