bdce5600912f86e813dd922c2ba130a93ecfa3ba
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-17 19:59:51 +0100

    Dinnye csomagoláshoz szükséges kalkulátor.

---
+++ CsomagoloCeg/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsomagoloCeg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dinnyeszam,
+                dinnyeatmero;
+
+            // Szalagnak kétszer körbe + 60 cm
+            Console.WriteLine("Dinnye átmérője: ");
+            dinnyeatmero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Dinnyék száma: ");
+            dinnyeszam = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(dinnyeszam + " darab dinnyéhez " + ((dinnyeatmero * 2) + 60) + " méter szalagra van szükség.");
+
+            Console.ReadKey();
+        }
+    }
+}
