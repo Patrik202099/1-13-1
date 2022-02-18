2615c485aeb73d212a279be618860c51796892c8
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-18 11:12:48 +0100

    Alapvető műveletek

---
+++ AlapvetoMuveletek/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlapvetoMuveletek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float elsoSzam,
+                masodikSzam;
+
+            Console.WriteLine("Első szám: ");
+            elsoSzam = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Második szám: ");
+            masodikSzam = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("A két szám összege: {0}", elsoSzam + masodikSzam);
+            Console.WriteLine("A két szám különbsége: {0}", elsoSzam - masodikSzam);
+            Console.WriteLine("A két szám szorzata: {0}", elsoSzam * masodikSzam);
+            Console.WriteLine("A két szám hányadosa: {0}", elsoSzam / masodikSzam);
+            Console.ReadKey();
+        }
+    }
+}
