b48fd510723d7a40b8b8eac9fd0dc5ef75823d6a
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-18 10:36:08 +0100

    Idopont kozotti kalkulator

---
+++ PercekEsOrak/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PercekEsOrak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan elso = new TimeSpan();
+            TimeSpan masodik = new TimeSpan();
+
+            Console.WriteLine("Első időpont: ");
+            elso = TimeSpan.Parse(Console.ReadLine());
+
+            Console.WriteLine("Második időpont: ");
+            masodik = TimeSpan.Parse(Console.ReadLine());
+
+            if (elso > masodik)
+            {
+                Console.WriteLine("A két időpont közötti különbség: " + (elso - masodik));
+            }
+
+            else if (masodik > elso)
+            {
+                Console.WriteLine("A két időpont közötti különbség: " + (masodik - elso));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
