f9637bb77202be593121425d434903d6551b347d
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-18 11:06:21 +0100

    Valós gyök kalkulátor

---
+++ Masofoku/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masofoku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a,
+                  b,
+                  c;
+
+            float valosgyok;
+            
+            Console.WriteLine("A: ");
+            a = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("B: ");
+            b = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("C: ");
+            c = float.Parse(Console.ReadLine());
+
+            valosgyok = (b * b - 4 * a * c);
+            Console.WriteLine("Valós gyök: " + valosgyok);
+
+            Console.ReadKey();
+        }
+    }
+}
