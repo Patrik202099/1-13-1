22c73a756d7da0af42d9c2e08feaaf2fe607bcb1
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-18 10:16:26 +0100

    Csempe kalkulátor

---
+++ Csaempezes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csaempezes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double teruletSzelesseg,
+                  magassag,
+                  szukseges,
+                  csempe = 0.2 * 0.2;
+
+            Console.WriteLine("Terület szélessége: ");
+            teruletSzelesseg = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Magasság méterben: ");
+            magassag = Convert.ToDouble(Console.ReadLine());
+
+            szukseges = (teruletSzelesseg * magassag) / csempe;
+
+            Console.WriteLine(szukseges + szukseges * 0.1 + "darab csempe szükséges.");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ sziget/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sziget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sziget felett 1-et, tenger felett 0-át 
+            // stringből
+
+            string data = "01101011110101010";
+            int szigetSzam,
+                szigetHossz;
+
+            
+        }
+    }
+}
