1e5d582d4e0151013d634ac5ad79ed6bbfe492a3
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-22 11:29:39 +0100

    majdnemLotto

---
+++ majdnemLotto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace majdnemLotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int counter = 0;
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 7);
+                Console.WriteLine(array[i]);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == 6)
+                {
+                    
+                    counter ++;
+                }
+            }
+            Console.WriteLine("Ennyiszer van benne: {0}", counter);
+            Console.ReadKey();
+        }
+        
+    }
+}
