b03c855c104ea376edd1cb61f6ac12f4004bdb15
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-22 11:36:21 +0100

    Szokozok

---
+++ Szokozok/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szokozok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mondat;
+
+            Console.WriteLine("Írj egy mondatot: ");
+            mondat = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine(String.Concat(mondat.Where(c => !Char.IsWhiteSpace(c))));
+            Console.ReadKey();
+        }
+    }
+}
