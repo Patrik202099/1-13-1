93a9cde5b92162f46efb942146d0a5dca278c47a
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-02-23 09:21:42 +0100

    Konyv

---
+++ Konyv/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konyv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int oldalszam;
+            string konyvcim;
+            bool terjedelem;
+
+            Console.WriteLine("Add meg a könyv címét: ");
+            konyvcim = Convert.ToString(Console.ReadLine());
+
+            while (string.IsNullOrEmpty(konyvcim))
+            {
+                Console.WriteLine("Add meg a könyv címét: ");
+                konyvcim = Convert.ToString(Console.ReadLine());
+            }
+
+            Console.WriteLine("Add meg az oldalak számát: ");
+            oldalszam = Convert.ToInt32(Console.ReadLine());
+
+            if (oldalszam > 150)
+            {
+                terjedelem = true;
+                Console.WriteLine("A/az " + konyvcim + " egy hosszú terjedelmű könyv.");
+            }
+
+            else if (oldalszam <= 150)
+            {
+                terjedelem = false;
+                Console.WriteLine("A/az " + konyvcim + " egy rövid terjedelmű könyv.");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
