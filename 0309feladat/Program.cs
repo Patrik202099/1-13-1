dcbb3f1dda700aa4b53b9b70e3fc104055dff876
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-03-09 11:40:06 +0100

    hang

---
+++ 0309feladat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0309feladat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //négyzet mátrix beolvasott méretek 2 átlót meghatározni
+            // 
+        }
+    }
+}
---
+++ hangman/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int eselyek = 0;
+            string helyek = "";
+            bool dontes = true;
+            char jatekosTalal;
+            Random rnd = new Random();
+            List<string> gondolas = new List<string> { "banán", "alma", "körte", "répa", "mogyoró", "szilva" };
+            string gepGondolt;
+
+            gepGondolt = gondolas[rnd.Next(0, 6)];
+
+            for (int i = 0; i < gepGondolt.Length; i++)
+            {
+                helyek += "_ ";
+            }
+
+            Console.WriteLine("A gép által gondolt szó: {0}", helyek);
+            
+
+            while (dontes)
+            {
+                Console.WriteLine("Gondolj egy betűre: ");
+                jatekosTalal = Convert.ToChar(Console.ReadKey());
+            }    
+
+            Console.ReadKey();
+        }
+    }
+}
