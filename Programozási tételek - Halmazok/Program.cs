2e35b0b76665520082a54bf67e595f0f26d7dfd5
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-03-18 10:21:45 +0100

    Programozási Tételek - Halmazok

--- Programozási tételek - Halmazok/Program.cs
+++ Programozási tételek - Halmazok/Program.cs
@@ -8,23 +8,69 @@ namespace Programozási_tételek___Halmazok
 {
     class Halmaz
     {
-        int elemSzam;
+        int[] A;
+        int[] B;
         Random rnd = new Random();
-        public void DarabSzamLetrehoz()
+        List<int> metszetSzam = new List<int>();
+
+        public Halmaz(int elemSzam)
         {
-            Console.WriteLine("Hány számot: (max 200)");
-            this.elemSzam = Convert.ToInt32(Console.ReadLine());
-            if (this.elemSzam > 200) { this.elemSzam = rnd.Next(100, 201); }
-            // Console.WriteLine(elemSzam);
+            this.A = new int [elemSzam];
+            this.B = new int [elemSzam];
         }
+
+        // A és B tömb létrehozása, majd számokkal való feltöltése.
         public void TombokLetrehoz()
         {
-            int[] A = { };
-            int[] B = { };
-            for (int i = 0; i < elemSzam; i++)
+            for (int i = 0; i < this.A.Length; i++)
             {
-                rnd.Next(-100, 100);
+                this.A[i] =this.rnd.Next(0, 5);
+                this.B[i] =this.rnd.Next(0, 5);
             }
+            foreach (var item in A)
+	        {
+                Console.WriteLine("A tömb elemei: {0}",item);
+	        }
+            Console.WriteLine("---------------");
+            foreach (var item in B)
+	        {
+                Console.WriteLine("B tömb elemei: {0}",item);
+	        } 
+            
+        }
+
+        public void aMinusB()
+        {   
+            var onlyA = this.A.Except(this.B);
+            Console.WriteLine("Csak az A-ban szereplő szám(ok): " + String.Join(", ", onlyA));
+        }
+
+        public void bMinusA()
+        {
+            var onlyB = this.B.Except(this.A);
+            Console.WriteLine("Csak a B-ben szereplő szám(ok): " + String.Join(", ", onlyB));
+        }
+
+        public void Unio()
+        {
+            var unio = this.A.Union(this.B);
+            Console.WriteLine("A két tömb uniója: " + String.Join(", ", unio));
+        }
+
+        public void Metszet()
+        {
+            
+            for (int i = 0; i < this.A.Length; i++)
+			{
+                for (int j = 0; j < this.B.Length; j++)
+			{
+                    if (this.A[i] == this.B[j]) 
+                    {
+                        Console.WriteLine("A metszete: {0}", A[i]);
+                    }
+			}
+
+			}
         }
 
     }
@@ -33,8 +79,17 @@ namespace Programozási_tételek___Halmazok
     {
         static void Main(string[] args)
         {
-            Halmaz h = new Halmaz();
-            h.DarabSzamLetrehoz();
+            Random rnd = new Random();
+            Console.WriteLine("Hány számot: (max 200)");
+            int elemSzam = Convert.ToInt32(Console.ReadLine());
+            if (elemSzam > 200) { elemSzam = rnd.Next(100, 201); }
+            Console.WriteLine("Választott elemek száma: {0}",elemSzam);
+            Halmaz h = new Halmaz(elemSzam);
+            h.TombokLetrehoz();
+            h.aMinusB();
+            h.bMinusA();
+            h.Unio();
+            h.Metszet();
             Console.ReadKey();
         }
     }
