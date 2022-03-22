3f58dfe29ec6cfa109025af9b1d598ce052ef0f2
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-03-22 18:24:06 +0100

    Password2

---
+++ Password2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int user, pass;
+            int counter = 0;
+
+            do
+            {
+                Console.WriteLine("User: ");
+                user = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Password: ");
+                pass = Convert.ToInt32(Console.ReadLine());
+
+                if (user != 12 && pass != 1234) 
+                { 
+                    Console.WriteLine("Error, try again!");
+                    counter++;
+                }
+
+            } while (user != 12 && pass != 1234 && counter != 3);
+
+            Console.WriteLine("Login success!");
+
+
+            Console.ReadKey();
+        }
+    }
+}
