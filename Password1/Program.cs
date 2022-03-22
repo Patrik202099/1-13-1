fe1dc4848fd4d1955ebe8d0e3e96e14e09bb3b19
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-03-22 18:21:54 +0100

    Password1

---
+++ Password1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int user, pass;
+
+            do
+            {
+                Console.WriteLine("User: ");
+                user = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Password: ");
+                pass = Convert.ToInt32(Console.ReadLine());
+
+                if (user != 12 && pass != 1234)     {  Console.WriteLine("Error, try again!");  }
+            }   while (user != 12 && pass != 1234);
+            
+                Console.WriteLine("Login success!");
+            
+
+            Console.ReadKey();
+        }
+    }
+}
