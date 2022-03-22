51b7d7ec3c5a7269ce712ac82a13fb2e7175c777
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-03-22 14:03:35 +0100

    asdasd

---
+++ Array1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] szamok = new int[5];
+            int bekSzam;
+
+            // Hozzáadás
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.WriteLine("Számot: ");
+                bekSzam = int.Parse(Console.ReadLine());
+                szamok[i] = bekSzam;
+
+            }
+
+            // Fordított kiíratás
+
+            for (int i = 5; i < szamok.Length; i--)
+            {
+                Console.WriteLine(szamok[i]);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
