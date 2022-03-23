e5a1fb6727ce0ebf79b5081a1c4704bbb9673155
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-03-23 09:10:18 +0100

    asd

---
+++ ArraySearch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int manyData;
+
+            Console.WriteLine("How many data: ");
+            manyData = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
