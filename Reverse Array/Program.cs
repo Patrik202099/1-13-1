8a8f10f74982428c9bd0b9b73b275a9421a02d8c
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-03-23 09:07:31 +0100

    ReversedArray

---
+++ Reverse Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[5];
+
+            // Add
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine("Numbers: ");
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            // Printing
+            for (int i= 4; i >= 0; i--)
+            {
+                Console.WriteLine("Number in reverse order: {0}", nums[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
