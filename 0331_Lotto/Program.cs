cd55698d0dd6e4c4f5c312bfd478bf06843e1bcf
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-03-31 10:00:14 +0200

    0331_Lotto

---
+++ 0331_Lotto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0331_Lotto
+{
+    class Lotto
+    {
+        public void szamGeneral()
+        {
+            int szamok;
+            int[] lottoSzamok = new int[5];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
