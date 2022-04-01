395227fca9fe95e495321eaa7acf10732fd55b77
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-04-01 10:50:20 +0200

    0401_Sorozatok

--- 0331_Sorozatok/Program.cs
+++ 0331_Sorozatok/Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace _0331_Sorozatok
 {
+    class Sorozatok
+    {
+        public void SorVizsgal()
+        {
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Sorozatok sor = new Sorozatok();
+            sor.SorVizsgal();
+            Console.ReadKey();
         }
     }
 }
