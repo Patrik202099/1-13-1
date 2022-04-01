dc6157ee764c78f1bf34fbb49907b401ad77a756
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-04-01 08:42:20 +0200

    asd

--- Homerseklet/Program.cs
+++ Homerseklet/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Homerseklet
 {
     public class Homerseklet(string asd)
---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int characterNum;
+            string [] array = { "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" };
+
+            Console.WriteLine("How many characters?: ");
+            characterNum = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
