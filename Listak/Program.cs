4cb694e89d68565a2c8d086edeae76fdb993bc70
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-04-04 18:34:17 +0200

    Listak

---
+++ Listak/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listak
+{
+    public class Listak
+    {
+        public void Feltoltes()
+        {
+            Random rnd = new Random();
+            List<int> szamLista = new List<int>();
+
+            // Lista feltöltése
+            for (int i = 0; i <= 10; i++)
+            {
+                szamLista.Add(rnd.Next(10, 100));
+            }
+
+            // Lista elemek kiíratása
+            foreach (var item in szamLista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Listak lista = new Listak();
+            lista.Feltoltes();
+            Console.ReadKey();
+        }
+    }
+}
