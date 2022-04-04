a7ab8c4a3cb5f3e7671ce3818f79d17e3a248d99
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-04-04 18:40:49 +0200

    Elagazas

---
+++ Elagazas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elagazas
+{
+    public class Elagazas
+    {
+        public void LoadAndRoll()
+        {
+            Random rnd = new Random();
+            List<int> szamosLista = new List<int>();
+
+            // Lista feltöltése, majd a páros elemek kiíratása.
+
+            for (int i = 0; i <= 10; i++)
+            {
+                szamosLista.Add(rnd.Next(1, 99));
+                if (szamosLista[i] % 2 == 0)
+                {
+                    Console.WriteLine("A lista páros elemei: {0}", szamosLista[i]);
+                }
+            }
+            
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Elagazas elagaz = new Elagazas();
+            elagaz.LoadAndRoll();
+            Console.ReadKey();
+        }
+    }
+}
