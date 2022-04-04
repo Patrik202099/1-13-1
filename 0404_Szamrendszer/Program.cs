2a02776998b34e3e0c841eb3ecf43262da92c8b7
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-04-04 13:12:08 +0200

    0404_Szamrendszer

---
+++ 0404_Szamrendszer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0404_Szamrendszer
+{
+    class Szamrendszer
+    {
+        public void Atvaltas()
+        {
+            int beker;
+            string binaris;
+            string oktan;
+            string hexadec;
+
+            Console.WriteLine("Adjon meg egy számot: ");
+            beker = Convert.ToInt32(Console.ReadLine());
+            binaris = Convert.ToString(beker, 2);
+            oktan = Convert.ToString(beker, 8);
+            hexadec = Convert.ToString(beker, 16);
+            Console.WriteLine("Az általad megadott szám bináris rendszerben: {0}",binaris);
+            Console.WriteLine("Az általad megadott szám oktán rendszerben: {0}",oktan);
+            Console.WriteLine("Az általad megadott szám hexadecimális rendszerben: {0}",hexadec);
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 10esből 2-esbe 8-asba 16-osba
+            Szamrendszer sz = new Szamrendszer();
+            sz.Atvaltas();
+            Console.ReadKey();
+        }
+    }
+}
