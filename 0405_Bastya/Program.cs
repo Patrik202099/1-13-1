672b7ffca83c8e477d74166b6135db555ceb0e9c
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-04-05 12:18:23 +0200

    0405_Bastya

---
+++ 0405_Bastya/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0405_Bastya
+{
+    public class Bastya
+    {
+        int[] tabla;
+        public Bastya()
+        {
+            this.tabla = new int[64];
+        }
+        public void Lehetosegek()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bastya bastie = new Bastya();
+            Console.ReadKey();
+        }
+    }
+}
