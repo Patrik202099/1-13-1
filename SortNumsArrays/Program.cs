8479a0889e8ade0158c34e7b5973ee40e70fdf35
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-04-29 08:48:38 +0200

    ArrayNumSort

---
+++ SortNumsArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortNumsArrays
+{
+    class ArrayNumSort
+    {
+        private int [] numArr;
+        public ArrayNumSort()
+        {   }
+        public void Sorting()
+        {
+            this.numArr = new int [10];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayNumSort ans = new ArrayNumSort();
+            Console.ReadKey();
+        }
+    }
+}
