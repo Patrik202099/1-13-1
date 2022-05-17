600457849ed20cf2f6db378fc43c9dc750e15461
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-05-17 11:13:36 +0200

    asd

---
+++ Hengerv2Teszt/HengerTeszt.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Hengerv2;
+
+namespace Hengerv2Teszt
+{
+    [TestClass]
+    public class HengerTeszt
+    {
+        [TestMethod]
+        public void KeruletTeszt()
+        {
+
+        }
+
+        public void TeruletTeszt()
+        {
+
+        }
+
+        public void FelszinTeszt()
+        {
+
+        }
+
+        public void TerfogatTeszt()
+        {
+
+        }
+    }
+}
---
+++ Hengerv2Teszt/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hengerv2Teszt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hengerv2Teszt")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("76ac36a0-6ecf-49b9-9cd6-4c3d448fe8ac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
