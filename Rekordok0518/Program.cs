69da763b2e756c0602cce03322069601f3156fe8
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-05-18 10:12:06 +0200

    Rekordok0518

---
+++ Rekordok0518/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Rekordok0518
+{
+    public class baratokAdatai
+    {
+        public string nev;
+        public string szulDatum;
+        public string nem;
+        public int buliHajlam;
+        public string[] sorok;
+        public List<string> beolvasottData;
+        public string fajlnev;
+
+        public baratokAdatai()
+        {
+            this.nev = "";
+            this.szulDatum = "";
+            this.nem = "";
+            this.buliHajlam = 0;
+            this.fajlnev = "";
+            this.sorok = new string[500000000];
+        }
+
+        public void Beolvas()
+        {
+            Console.WriteLine("Add meg a fájl nevét: ");
+            this.fajlnev = Convert.ToString(Console.ReadLine());
+            this.sorok = File.ReadAllLines(@"C:\Users\2021306\Source\Repos\Patrik202099\1-13-1\Rekordok0518\" + this.fajlnev);
+        }
+
+        public void Kiir()
+        {
+            for (int i = 0; i < this.sorok.Length; i++)
+            {
+                Console.WriteLine(sorok[i]);
+            }
+            Console.WriteLine("Beolvasás befejezve.");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            baratokAdatai baratRekord = new baratokAdatai();
+            baratRekord.Beolvas();
+            baratRekord.Kiir();
+
+            Console.ReadKey();
+        }
+    }
+}
