023179cc1dd109182e37121d970c9c635274f5c2
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-05-30 15:16:24 +0200

    05_30_Commit

---
+++ Sziget/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sziget
+{
+    public class SzigetHosszok
+    {
+        string szigetErtekek;
+
+        public SzigetHosszok()
+        {
+            this.szigetErtekek = "0111001100";
+        }
+
+        public int szigetDrbSz()
+        {
+
+        }
+
+        public int hosszVizsgalat()
+        {
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SzigetHosszok szigetHossz = new SzigetHosszok();
+            Console.WriteLine("Szigetek száma: " + szigetHossz.szigetDrbSz());
+            Console.WriteLine("Leghosszabb sziget hossza: "+ szigetHossz.hosszVizsgalat());
+
+            Console.ReadKey();
+        }
+    }
+}
