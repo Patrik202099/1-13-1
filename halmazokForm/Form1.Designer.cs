60d7a8db1c5b728ab588ab169eca9b43d9f25fa1
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-06-01 13:01:22 +0200

    06_01_Commit

---
+++ halmazokForm/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace halmazokForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.elsoSzam = new System.Windows.Forms.TextBox();
+            this.masodikSzam = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // elsoSzam
+            // 
+            this.elsoSzam.Location = new System.Drawing.Point(80, 452);
+            this.elsoSzam.Name = "elsoSzam";
+            this.elsoSzam.Size = new System.Drawing.Size(100, 20);
+            this.elsoSzam.TabIndex = 0;
+            // 
+            // masodikSzam
+            // 
+            this.masodikSzam.Location = new System.Drawing.Point(196, 452);
+            this.masodikSzam.Name = "masodikSzam";
+            this.masodikSzam.Size = new System.Drawing.Size(100, 20);
+            this.masodikSzam.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 416);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "A halmazok elemszámai:";
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(316, 450);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(75, 23);
+            this.submitBtn.TabIndex = 4;
+            this.submitBtn.Text = "Előállít";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(35, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 316);
+            this.listBox1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(176, 51);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 316);
+            this.listBox2.TabIndex = 6;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(316, 51);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 316);
+            this.listBox3.TabIndex = 7;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(461, 51);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(120, 316);
+            this.listBox4.TabIndex = 8;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(601, 51);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(120, 316);
+            this.listBox5.TabIndex = 9;
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(744, 51);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(120, 316);
+            this.listBox6.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "A:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(313, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "A unio B:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(458, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "A metszet B:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(598, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "A-B:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(741, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "B-A:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(173, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "B:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 551);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.masodikSzam);
+            this.Controls.Add(this.elsoSzam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox elsoSzam;
+        private System.Windows.Forms.TextBox masodikSzam;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ halmazokForm/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace halmazokForm
+{
+    public partial class Form1 : Form
+    {
+        int [] aElemek;
+        int [] bElemek;
+        int[] aUnioB;
+        int[] aMetszetB;
+        int[] aMinusB;
+        int[] bMinusA;
+        int aElem;
+        int bElem;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.aElemek = new int[99];
+            this.bElemek = new int[99];
+            this.aUnioB = new int[99];
+            this.aMetszetB = new int[99];
+            this.aMinusB = new int[99];
+            this.bMinusA = new int[99];
+            this.aElem = 0;
+            this.bElem = 0;
+        }
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            this.aElem = Convert.ToInt32(elsoSzam.Text);
+            this.bElem = Convert.ToInt32(masodikSzam.Text);
+
+            if (this.aElem < 10) { this.aElem = 10; }
+            else if (this.aElem > 100) { this.aElem = 100; }
+
+            if (this.bElem < 10) { this.bElem = 10; }
+            else if (this.bElem > 100) { this.bElem = 100; }
+
+            // A elemek generálása, feltöltése
+
+            for (int i = 0; i < this.aElem; i++)
+            {
+                listBox1.Items.Add(this.aElemek[i] = rnd.Next(100));
+            }
+
+            // B elemek generálása, feltöltése
+
+            for (int y = 0; y < this.bElem; y++)
+            {
+                listBox2.Items.Add(this.bElemek[y] = rnd.Next(100));
+            }
+        }
+    }
+}
