4273725728d969f1ec3e9274bf1d6d2eb375c34c
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-06-01 09:45:10 +0200

    06_01_Commit

---
+++ KepetForgat/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace KepetForgat
+{
+    partial class kepForgat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(kepForgat));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(538, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 105);
+            this.label2.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
+            this.label4.Location = new System.Drawing.Point(520, 371);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 103);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
+            this.label5.Location = new System.Drawing.Point(380, 467);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 107);
+            this.label5.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+            this.label3.Location = new System.Drawing.Point(595, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 102);
+            this.label3.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.Image = ((System.Drawing.Image)(resources.GetObject("label6.Image")));
+            this.label6.Location = new System.Drawing.Point(230, 371);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 103);
+            this.label6.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.Image = ((System.Drawing.Image)(resources.GetObject("label7.Image")));
+            this.label7.Location = new System.Drawing.Point(166, 240);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 106);
+            this.label7.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.Image = ((System.Drawing.Image)(resources.GetObject("label8.Image")));
+            this.label8.Location = new System.Drawing.Point(227, 119);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(106, 105);
+            this.label8.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(380, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 105);
+            this.label1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(119, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 69);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(686, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 69);
+            this.button2.TabIndex = 11;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // kepForgat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(825, 632);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Name = "kepForgat";
+            this.Text = "Képforog";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ KepetForgat/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KepetForgat
+{
+    public partial class kepForgat : Form
+    {
+        public kepForgat()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image a = label7.Image;
+            label7.Image = label8.Image;
+            label8.Image = label6.Image;
+            label6.Image = label5.Image;
+            label4.Image = label3.Image;
+            label2.Image = label1.Image;
+           
+            label1.Image = a;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Image a = label7.Image;
+            label7.Image = label8.Image;
+            label8.Image = label6.Image;
+            label6.Image = label5.Image;
+            label4.Image = label3.Image;
+            label2.Image = label1.Image;
+
+            label1.Image = a;
+        }
+    }
+}
