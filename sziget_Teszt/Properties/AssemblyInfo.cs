59af6a7eaf80e7b2393816af4eab4ef0298cf143
Patrik Tóth <2021306@dszcberegszaszi.hu>
2022-06-09 09:07:55 +0200

    06_09_Commit

---
+++ sziget_Teszt/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("sziget_Teszt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sziget_Teszt")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5116f4c2-2e39-4775-a3a6-f9b4d3f1ab71")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ sziget_Teszt/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using sziget;
+
+namespace szigetTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Sziget s = new Sziget();
+            int varteredmeny = 5;
+            int kapotteredmeny = s.szigethossz();
+
+            Assert.AreEqual(varteredmeny, kapotteredmeny);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Sziget s = new Sziget();
+            int varteredmeny1 = 4;
+            int kapotteredmeny1 = s.szigetszam();
+
+            Assert.AreEqual(varteredmeny1, kapotteredmeny1);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Sziget s = new Sziget();
+            int varteredmeny = 5;
+            int kapotteredmeny = s.szigethossz();
+
+            Assert.AreNotEqual(varteredmeny, kapotteredmeny);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Sziget s = new Sziget();
+            int varteredmeny1 = 4;
+            int kapotteredmeny1 = s.szigetszam();
+
+            Assert.AreNotEqual(varteredmeny1, kapotteredmeny1);
+        }
+    }
+}
